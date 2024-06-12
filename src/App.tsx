3e44dfbb79fd865eaf489bb9976fcc02be3ce67a
atheekalm <atheekmohamad@gmail.com>
2024-06-12 19:43:53 +0530

    ga4

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { MainPage } from "./app/MainPage";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+  
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <MainPage />;
     </BrowserRouter>
   );
